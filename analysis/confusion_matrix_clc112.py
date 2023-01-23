2d35b578c4b2489693a6f090d5f1ff75b24e178d
Vangelis Fotakidis <vfotakidis96@gmail.com>
2023-01-23 10:04:01 +0200

    v0.7.0

--- analysis/confusion_matrix_clc112.py
+++ analysis/confusion_matrix_clc112.py
@@ -38,6 +38,9 @@ def get_geotransformation(metadata_src):
 def save_classified(ref, brp, width, height, transform, crs, output_path):
     cla = brp
 
+    brp = np.where(ref > 112, -1, brp)
+    ref = np.where(ref != 112, 0, ref)
+
     cla = np.where((ref == 0) & (brp == -1), 1, cla)
     cla = np.where((ref > 0) & (brp == -1), -1, cla)
     cla = np.where((ref > 0) & (brp > 0), 2, cla)
@@ -60,6 +63,15 @@ def save_classified(ref, brp, width, height, transform, crs, output_path):
 
 def compute_error_matrix(ref, brp, emsr, idx, output):
     # All studied pixels
+    # unburned_unburned = np.count_nonzero((ref == 0) & (brp == -1))
+    # burned_unburned = np.count_nonzero((ref > 0) & (brp == -1))
+    # burned_burned = np.count_nonzero((ref > 0) & (brp > 0))
+    # unburned_burned = np.count_nonzero((ref == 0) & (brp > 0))
+
+    # Only for specific class of pixels (ref|clc==112)
+    brp = np.where(ref > 112, -1, brp)
+    ref = np.where(ref != 112, 0, ref)
+
     unburned_unburned = np.count_nonzero((ref == 0) & (brp == -1))
     burned_unburned = np.count_nonzero((ref > 0) & (brp == -1))
     burned_burned = np.count_nonzero((ref > 0) & (brp > 0))
@@ -73,43 +85,78 @@ def compute_error_matrix(ref, brp, emsr, idx, output):
 
     total_pixels = ref_unburned + ref_burned
 
-    PA_burned = np.round(burned_burned / ref_burned, 3)
-    PA_unburned = np.round(unburned_unburned / ref_unburned, 3)
-    UA_burned = np.round(burned_burned / class_burned, 3)
-    UA_unburned = np.round(unburned_unburned / class_unburned, 3)
+    print(class_burned, class_unburned, ref_burned, ref_unburned)
+    if ref_burned>0:
+        try:
+            PA_burned = np.round(burned_burned / ref_burned, 3)
+            PA_unburned = np.round(unburned_unburned / ref_unburned, 3)
+            UA_burned = np.round(burned_burned / class_burned, 3)
+            UA_unburned = np.round(unburned_unburned / class_unburned, 3)
 
-    OA = np.round((burned_burned + unburned_unburned) / total_pixels, 3)
-    AC = np.round(ref_burned / total_pixels * class_burned / total_pixels +
-                  ref_unburned / total_pixels * class_unburned / total_pixels, 3)
-    Kappa = np.round((OA - AC) / (1 - AC), 3)
+            OA = np.round((burned_burned + unburned_unburned) / total_pixels, 3)
+            AC = np.round(ref_burned / total_pixels * class_burned / total_pixels +
+                          ref_unburned / total_pixels * class_unburned / total_pixels, 3)
+            Kappa = np.round((OA - AC) / (1 - AC), 3)
+        except ZeroDivisionError:
+            PA_burned = np.round(burned_burned / ref_burned, 3)
+            PA_unburned = np.round(unburned_unburned / ref_unburned, 3)
+            UA_burned = 0
+            UA_unburned = np.round(unburned_unburned / class_unburned, 3)
 
-    df = pd.DataFrame({'emsr': emsr,
-                       'index': idx,
-                       'burned_burned': burned_burned,
-                       'burned_unburned': burned_unburned,
-                       'unburned_burned': unburned_burned,
-                       'unburned_unburned': unburned_unburned,
-                       'class_burned': class_burned,
-                       'class_unburned': class_unburned,
-                       'ref_burned': ref_burned,
-                       'ref_unburned': ref_unburned,
-                       'total_pixels': total_pixels,
-                       'PA_b': PA_burned,
-                       'UA_b': UA_burned,
-                       'PA_u': PA_unburned,
-                       'UA_u': UA_unburned,
-                       'OA': OA,
-                       'Kappa': Kappa}, index=[0])
+            OA = np.round((burned_burned + unburned_unburned) / total_pixels, 3)
+            AC = np.round(ref_burned / total_pixels * class_burned / total_pixels +
+                          ref_unburned / total_pixels * class_unburned / total_pixels, 3)
+            Kappa = np.round((OA - AC) / (1 - AC), 3)
 
-    df.to_csv(output, sep=',', index=False)
+        df = pd.DataFrame({'emsr': emsr,
+                           'index': idx,
+                           'burned_burned': burned_burned,
+                           'burned_unburned': burned_unburned,
+                           'unburned_burned': unburned_burned,
+                           'unburned_unburned': unburned_unburned,
+                           'class_burned': class_burned,
+                           'class_unburned': class_unburned,
+                           'ref_burned': ref_burned,
+                           'ref_unburned': ref_unburned,
+                           'total_pixels': total_pixels,
+                           'PA_b': PA_burned,
+                           'UA_b': UA_burned,
+                           'PA_u': PA_unburned,
+                           'UA_u': UA_unburned,
+                           'OA': OA,
+                           'Kappa': Kappa}, index=[0])
 
-    return df.to_dict('records')
+        df.to_csv(output, sep=',', index=False)
+
+        return df.to_dict('records')
+    else:
+        df = pd.DataFrame({'emsr': emsr,
+                           'index': idx,
+                           'burned_burned': burned_burned,
+                           'burned_unburned': burned_unburned,
+                           'unburned_burned': unburned_burned,
+                           'unburned_unburned': unburned_unburned,
+                           'class_burned': '#NA',
+                           'class_unburned': '#NA',
+                           'ref_burned': '#NA',
+                           'ref_unburned': '#NA',
+                           'total_pixels': '#NA',
+                           'PA_b': '#NA',
+                           'UA_b': '#NA',
+                           'PA_u': '#NA',
+                           'UA_u': '#NA',
+                           'OA': '#NA',
+                           'Kappa': '#NA'}, index=[0])
+
+        df.to_csv(output, sep=',', index=False)
+
+        return df.to_dict('records')
 
 
 def main():
     # args = get_sys_argv()
     # arg_json_file = args['json_file']
-    arg_json_file = "E:/Publications/BFAST_Monitor/results/emsr/zonal_stats_ras2ras/rasta_code18_dnbr_rbr_nbr360_urban.json"
+    arg_json_file = "D:/Publications/BFAST_Monitor/results/emsr/zonal_stats_ras2ras/rasta_code18_dnbr_rbr_nbr360_urban.json"
 
     with open(arg_json_file) as f:
         parameters_dict = json.load(f)
